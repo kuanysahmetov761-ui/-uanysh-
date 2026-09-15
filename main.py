fea330599f9df985ab21c35e12db4dfa315a52a9
kuanysahmetov761-ui <kuanysahmetov761@gmail.com>
2026-09-15 12:04:29 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import telebot, json, os, threading
 from datetime import datetime
 from flask import Flask
 
-TOKEN = os.getenv("BOT_TOKEN"
+TOKEN = os.getenv("BOT_TOKEN")
 bot = telebot.TeleBot(TOKEN)
 FILE = 'users_data.json'
 
